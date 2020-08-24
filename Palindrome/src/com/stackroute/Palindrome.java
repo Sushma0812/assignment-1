976d5a9502f942019eeb6faf91daf76cd8f483b2
Sushma Bhupathi <bhupathisushma1998@gmail.com>
2020-08-24 10:11:57 +0000

    ASsignment programs

---
+++ Palindrome/src/com/stackroute/Palindrome.java
@@ -0,0 +1,33 @@
+package com.stackroute;
+import java.util.Scanner;
+public class Palindrome {
+    public static int GetInput() {
+        s.
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter a number");
+        int num=int GetInput();
+        void Positive(int num);
+        void Valid(int num);
+        void Palindrome(int num);
+        void Display();
+
+
+    }
+}
